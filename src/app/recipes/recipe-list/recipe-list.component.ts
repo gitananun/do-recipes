8ee8b7fbd7f50e862a29ff8915f0badba2f52b12
gitananun <tigran.muradyan140201@gmail.com>
2022-05-26 12:55:40 +0400

    Recipes list component

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,15 +1,26 @@
 import { Component, OnInit } from '@angular/core';
+import { Recipe } from '../recipe.model';
 
 @Component({
   selector: 'app-recipe-list',
   templateUrl: './recipe-list.component.html',
-  styleUrls: ['./recipe-list.component.css']
+  styleUrls: ['./recipe-list.component.css'],
 })
 export class RecipeListComponent implements OnInit {
+  recipes: Recipe[] = [
+    new Recipe(
+      'Manti',
+      'Manti is a type of dumpling popular in most Turkic cuisines, as well as in the cuisines of the South Caucasus and Balkans.',
+      'https://img.chefkoch-cdn.de/rezepte/1808801292881984/bilder/949888/crop-960x720/kayseri-manti.jpg'
+    ),
+    new Recipe(
+      'Manti',
+      'Manti is a type of dumpling popular in most Turkic cuisines, as well as in the cuisines of the South Caucasus and Balkans.',
+      'https://img.chefkoch-cdn.de/rezepte/1808801292881984/bilder/949888/crop-960x720/kayseri-manti.jpg'
+    ),
+  ];
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
